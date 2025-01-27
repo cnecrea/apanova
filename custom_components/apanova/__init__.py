189b4e338481e74a6f7e5c80d3598a00ef990040
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2025-01-27 03:38:46 +0200

    Create __init__.py

---
+++ custom_components/apanova/__init__.py
@@ -0,0 +1,49 @@
+"""Inițializarea integrării Apanova România."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, LOGGER, CONF_CITY, CONF_EMAIL, CONF_PASSWORD, CONF_COD_CLIENT, CONF_UPDATE_INTERVAL
+from .coordinator import ApanovaCoordinator
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    # Configurează integrarea Apanova dintr-un config entry
+    LOGGER.debug("Se inițializează Apanova România...")
+
+    city = config_entry.data[CONF_CITY]
+    email = config_entry.data[CONF_EMAIL]
+    password = config_entry.data[CONF_PASSWORD]
+    cod_client = config_entry.data[CONF_COD_CLIENT]
+    update_interval = config_entry.options.get(CONF_UPDATE_INTERVAL)
+
+    # Creăm coordinatorul
+    coordinator = ApanovaCoordinator(
+        hass,
+        city,
+        email,
+        password,
+        cod_client,
+        update_interval
+    )
+    # Primul refresh
+    await coordinator.async_config_entry_first_refresh()
+
+    # Stocăm coordinatorul pentru acces în alte fișiere
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = coordinator
+
+    # Încărcăm platformele (sensor)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    # Descărcăm o config entry
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+    return unload_ok
